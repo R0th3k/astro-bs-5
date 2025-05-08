b79f1ac9eb0d965f2c5132372e4df1c00140bb12
Héktor Peña <dev@hektor.mx>
2025-05-07 22:32:44 -0600

    Plantilla base

---
+++ astro.config.mjs
@@ -0,0 +1,27 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import vue from '@astrojs/vue';
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+site: 'https://viveydescubremexico.com//', 
+base: '/', // Configuración base
+integrations: [mdx(), vue(), sitemap()], 
+vite: {
+	build: {
+	rollupOptions: {
+		output: {
+		assetFileNames: (assetInfo) => {
+			// Personalización del nombre de los archivos CSS
+			if (assetInfo.name === 'index.css') {
+			return 'assets/css/main.css';
+			}
+			return 'assets/[name][extname]';
+		},
+		},
+	},
+	},
+},
+});
---
+++ src/consts.ts
@@ -0,0 +1,7 @@
+export const SITE_TITLE = 'Hektor';
+export const SITE_DESCRIPTION = '';
+export const SITE_SINCE = '2000';
+
+export const patrocinadores = [
+    { img: "patrocinador.svg", url: "https://www.hektor.mx/" },
+  ];
