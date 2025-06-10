13a36476524215cd3be5e5962b8def300260af54
Hektor Peña <dev@hektor.mx>
2025-06-09 21:35:30 -0600

    Mejoras en layouts / componentes / estilos

--- src/consts.ts
+++ src/consts.ts
@@ -1,14 +1,10 @@
   export const URLWEB = import.meta.env.BASE_URL;
 
-export const SITE_TITLE = 'Hektor';
-export const SITE_DESCRIPTION = '';
+export const SITE_TITLE = 'ASTRO BS 5';
+export const SITE_DESCRIPTION = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem possimus doloribus ad minima molestiae, esse dolores facere illo doloremque ratione ipsa, nemo ab unde odio molestias dolorum reiciendis quibusdam rerum!';
 export const SITE_SINCE = '2000';
 
 export const MAIN_NAV = [
     { name: "Inicio", href: "", isAnchor: false },
     { name: "Blog", href: "blog", isAnchor: false },
     ];
-
-export const patrocinadores = [
-    { img: "patrocinador.svg", url: "https://www.hektor.mx/" },
-  ];
