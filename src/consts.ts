b4d941d6087955c571a244d679b67180fee9ea9f
Hektor Peña <dev@hektor.mx>
2025-06-09 20:42:23 -0600

    Componente Image

--- src/consts.ts
+++ src/consts.ts
@@ -1,11 +1,11 @@
-export const URLWEB = import.meta.env.BASE_URL;
+  export const URLWEB = import.meta.env.BASE_URL;
 
 export const SITE_TITLE = 'Hektor';
 export const SITE_DESCRIPTION = '';
 export const SITE_SINCE = '2000';
 
 export const MAIN_NAV = [
-    { name: "Inicio", href: "/", isAnchor: false },
+    { name: "Inicio", href: "", isAnchor: false },
     { name: "Blog", href: "blog", isAnchor: false },
     ];
 
