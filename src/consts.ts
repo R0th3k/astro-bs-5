69c7df81d86bd954ab947ee834b321e7ecbbf592
Hektor Peña <dev@hektor.mx>
2025-06-09 20:03:43 -0600

    Mejora del componente navbar

--- src/consts.ts
+++ src/consts.ts
@@ -4,6 +4,11 @@ export const SITE_TITLE = 'Hektor';
 export const SITE_DESCRIPTION = '';
 export const SITE_SINCE = '2000';
 
+export const MAIN_NAV = [
+    { name: "Inicio", href: "/", isAnchor: false },
+    { name: "Blog", href: "blog", isAnchor: false },
+    ];
+
 export const patrocinadores = [
     { img: "patrocinador.svg", url: "https://www.hektor.mx/" },
   ];
